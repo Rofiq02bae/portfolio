fea5970981fc550ac270068fd51425a5d925cbb4
As'adur Rofiq <145300940+Rofiq02bae@users.noreply.github.com>
2025-06-28 15:04:53 +0000

    Move project to root

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,27 @@
-<!-- <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import Hero from './components/Hero.vue'
+import About from './components/About.vue'
+import Projects from './components/Projects.vue'
+import Services from './components/Services.vue'
+import Skills from './components/Skills.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <Hero />
+    <Skills />
+    <Services />
+    <About />
+    <Projects />
+    <Footer />
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+main {
+  font-family: 'Segoe UI', sans-serif;
+  background: #0e0e0e;
+  color: #f0f0f0;
 }
 </style>
- -->
-
- <template>
-   <div>
-     <Header />
-     <Hero />
-     <Skills />
-     <Services />
-     <About />
-     <Projects />
-     <Footer />
-   </div>
- </template>
-
- <script setup>
- import Header from './components/Header.vue'
- import Hero from './components/Hero.vue'
- import Skills from './components/Skills.vue'
- import Services from './components/Services.vue'
- import About from './components/About.vue'
- import Projects from './components/Projects.vue'
- import Footer from './components/Footer.vue'
- </script>
